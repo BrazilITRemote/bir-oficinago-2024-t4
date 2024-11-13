780b47d7218ccf28762edec3c49330aee862d8b1
Felipe Alves <felipe.rod.alves@gmail.com>
2024-11-12 22:37:35 -0300

    feat: add language selection to the Hello function

--- estudantes/feliperodalves/learning-go-with-tests/hello-world/hello.go
+++ estudantes/feliperodalves/learning-go-with-tests/hello-world/hello.go
@@ -2,15 +2,28 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const spanish = "Spanish"
+const french = "French"
 
-func Hello(name string) string {
+const englishHelloPrefix = "Hello, "
+const spanishHellpPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == spanish {
+		return spanishHellpPrefix + name
+	}
+	if language == french {
+		return frenchHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Felipe"))
+	fmt.Println(Hello("Felipe", ""))
 }
