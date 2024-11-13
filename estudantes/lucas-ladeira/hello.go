f074d084046953e838c0beaaf75eaa1572c1b35e
lucas-ladeira <lucas.bellinazzi@gmail.com>
2024-11-12 20:41:54 -0500

    Adicionando constantes

--- estudantes/lucas-ladeira/hello.go
+++ estudantes/lucas-ladeira/hello.go
@@ -7,18 +7,25 @@ const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
+	func Hello(name string, language string) string {
+		if name == "" {
+			name = "World"
+		}
+	
+		return greetingPrefix(language) + name
 	}
-	if language == spanish {
-		return spanishHelloPrefix + name
+	
+	func greetingPrefix(language string) (prefix string) {
+		switch language {
+		case french:
+			prefix = frenchHelloPrefix
+		case spanish:
+			prefix = spanishHelloPrefix
+		default:
+			prefix = englishHelloPrefix
+		}
+		return
 	}
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
-}
 
 func main() {
 	fmt.Println(Hello("Lucas", ""))
