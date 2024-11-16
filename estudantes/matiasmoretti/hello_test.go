6fe8104df513f640f79c421bb48479297863c204
Matias Moretti <matiassmoretti@gmail.com>
2024-11-16 10:49:54 -0500

    #3 commit

--- estudantes/matiasmoretti/hello_test.go
+++ estudantes/matiasmoretti/hello_test.go
@@ -5,19 +5,30 @@ import "testing"
 func TestHello(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
 
-		got := Hello("Matias")  // Não tenho o codigo ainda e ja estou pensando no futuro
-		want := "Hello, Matias" //O que eu quero que aconteça no futuro
+		got := Hello("Matias", "") // Não tenho o codigo ainda e ja estou pensando no futuro
+		want := "Hello, Matias"    //O que eu quero que aconteça no futuro
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 
 	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Eloide", "Spanish")
+		want := "Hola, Eloide"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Francis", "French")
+		want := "Bonjour, Francis"
+		assertCorrectMessage(t, got, want)
+	})	
 
 }
 
