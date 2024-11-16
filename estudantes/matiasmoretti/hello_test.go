1acdb56257a05efa6fb7056241ecc54b881d0e7b
Matias Moretti <matiassmoretti@gmail.com>
2024-11-16 10:10:05 -0500

    #1 Commit

---
+++ estudantes/matiasmoretti/hello_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("Saying hello to people", func(t *testing.T) {
+
+		got := Hello("Matias")  // Não tenho o codigo ainda e ja estou pensando no futuro
+		want := "Hello, Matias" //O que eu quero que aconteça no futuro
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
