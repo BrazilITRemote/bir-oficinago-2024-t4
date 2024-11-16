2a3593bda42d5d393d5999a323225709a07145e9
Matias Moretti <matiassmoretti@gmail.com>
2024-11-16 10:25:05 -0500

    #2 Commit

--- estudantes/matiasmoretti/hello_test.go
+++ estudantes/matiasmoretti/hello_test.go
@@ -15,9 +15,15 @@ func TestHello(t *testing.T) {
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
 	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
