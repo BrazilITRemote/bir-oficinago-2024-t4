36d4d5e1b26e854fa68fc660eedaa810dd5b1711
Yago Santos <yagosansz@gmail.com>
2024-11-17 00:42:06 -0500

    feat: add func SumAllTails

--- estudantes/yagosansz/04-arrays_slices/sum.go
+++ estudantes/yagosansz/04-arrays_slices/sum.go
@@ -9,13 +9,23 @@ func Sum(numbers []int) (sum int) {
 
 // Go lets us write variadic functions,
 // which means a func can take a variable number of arguments
-func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] += Sum(numbers)
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 
-	return sums
+	return
+}
+
+// Sums all items in a collection, except for the first one (the "head")
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:] // slice[low:high]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return
 }
