08c33c3ee8489d990afeb22a8459c14914c4a7d1
italo-gus <italogus@gmail.com>
2024-11-18 21:19:21 -0300

    010 helloworld  Switch, declaração de variáveis (constantes) em bloco

--- estudantes/italo-gus/helloworld/hello_test.go
+++ estudantes/italo-gus/helloworld/hello_test.go
@@ -1,4 +1,4 @@
-package main // TDD com subtestes go test -v para expor relatório dos testes
+package main // switch, declaração de variáveis (constantes) em bloco
 
 import (
 	"testing"
@@ -28,29 +28,7 @@ func TestHello(t *testing.T) {
 }
 func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
-	/*
-		Helper marca a função de chamada como uma função de teste auxiliar.
-		Ao imprimir informações de arquivo e linha, essa função será ignorada.
-		Helper pode ser chamado simultaneamente de várias goroutines.
-		https://pkg.go.dev/testing#T.Helper
-	*/
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
-
-/*
-no terminal go test -v para expor relatório dos testes
-=== RUN   TestHello
-=== RUN   TestHello/diz_olá_para_as_pessoas
-=== RUN   TestHello/'Mundo'_como_padrão_para_'string'_vazia
-=== RUN   TestHello/em_espanhol
-=== RUN   TestHello/em_francês
---- PASS: TestHello (0.00s)
-    --- PASS: TestHello/diz_olá_para_as_pessoas (0.00s)
-    --- PASS: TestHello/'Mundo'_como_padrão_para_'string'_vazia (0.00s)
-    --- PASS: TestHello/em_espanhol (0.00s)
-    --- PASS: TestHello/em_francês (0.00s)
-PASS
-ok      learn-go-with-tests/helloworld_07
-*/
