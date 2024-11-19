f10b7b9182657213e8318670d50358433ff5dd8c
italo-gus <italogus@gmail.com>
2024-11-18 21:15:22 -0300

    008 helloworld  TDD com subtestes e IF

--- estudantes/italo-gus/helloworld/hello.go
+++ estudantes/italo-gus/helloworld/hello.go
@@ -1,13 +1,19 @@
-package main // const
+package main // TDD com subtestes e IF
 
 import "fmt"
 
 const englishHelloPrefix = "Hello, " // Definindo constante
 
 func Hello(name string) string {
+	if name == "" { // teste de condição ( se condição { realize } )
+		name = "World"
+	}
+	/* https://go.dev/doc/effective_go#if
+	https://go.dev/ref/spec#If_statements */
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World"))
 }
