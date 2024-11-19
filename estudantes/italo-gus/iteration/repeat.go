f0da03c8c18249028d025a294790a98a8141b91d
italo-gus <italogus@gmail.com>
2024-11-18 21:32:35 -0300

    015 iteration  Aplicando TDD com Benchmarks em Go

--- estudantes/italo-gus/iteration/repeat.go
+++ estudantes/italo-gus/iteration/repeat.go
@@ -1,4 +1,4 @@
-package iteration // Refactor
+package iteration // Benchmarks em Go
 
 const repeatCount = 5
 
@@ -6,8 +6,6 @@ func Repeat(character string) string {
 	var repeated string = ""
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
-		/* += Concatenação de strings
-		https://go.dev/ref/spec#String_concatenation */
 	}
 	return repeated
 }
--- estudantes/italo-gus/iteration/repeat_test.go
+++ estudantes/italo-gus/iteration/repeat_test.go
@@ -1,4 +1,4 @@
-package iteration // Refactor
+package iteration // Benchmarks em Go
 
 import "testing"
 
@@ -10,3 +10,15 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected '%q' but got '%q'", expected, repeated)
 	}
 }
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+
+/* benchmarks em Go
+https://pkg.go.dev/testing#hdr-Benchmarks
+Para executar os benchmarks go test -bench=.
+(no Windows Powershell go test -bench=".")
+*/
