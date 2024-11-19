bf98c234f8506b598f9ba918bbb5c324897a6d8d
italo-gus <italogus@gmail.com>
2024-11-18 21:41:17 -0300

    017 arrays Refactor - Refatoração / for range / _ blank identifier (identificador em branco)

--- estudantes/italo-gus/arrays/sum.go
+++ estudantes/italo-gus/arrays/sum.go
@@ -1,10 +1,16 @@
-package arrays // Arrays
+package arrays // Refactor - Refatoração / for range / _ blank identifier (identificador em branco)
 
 // Sum retona o cálculo da somas dos elementos de uma matriz de 5 números inteiros.
 func Sum(numbers [5]int) int {
 	var sum int = 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
+
+/*	Iteração / Laços de Repetições utilizando range
+	https://go.dev/doc/effective_go#for
+	_ blank identifier (identificador em branco)
+	https://go.dev/doc/effective_go#blank
+*/
--- estudantes/italo-gus/arrays/sum_test.go
+++ estudantes/italo-gus/arrays/sum_test.go
@@ -1,22 +1,15 @@
-package arrays // Arrays
+package arrays // Refactor - Refatoração / for range / _ blank identifier (identificador em branco)
 
 import "testing"
 
 func TestSum(t *testing.T) {
 	numbers := [5]int{1, 2, 3, 4, 5}
-	/*	var nomeDaArray = [capacidade] {valor1, valorn}
-		Arrays têm uma capacidade fixa que você define quando declara a variável,
-		podendo ser com a capacidade inferida
-		nomeDaArray := [...] {valor1, valorn}
-		https://go.dev/doc/effective_go#arrays
-		https://go.dev/ref/spec#Array_types
-	*/
+
 	repeated := Sum(numbers)
 	expected := 15
 
 	if repeated != expected {
 		t.Errorf("expected '%d' but repeated '%d' given, '%v'", expected, repeated, numbers)
 	}
-	/* %q imprimir strings, %d imprimir inteiro, %v imprimir o valor em um formato padrão.
-	Formatação de erros https://pkg.go.dev/fmt#hdr-Printing	*/
+
 }
