035974c6e2cc4fbb17f75a4fe50c0dbaac3ead84
Yago Santos <yagosansz@gmail.com>
2024-11-30 19:07:01 -0500

    refactor: add Rectangle struct

--- estudantes/yagosansz/05-interfaces/shapes.go
+++ estudantes/yagosansz/05-interfaces/shapes.go
@@ -1,9 +1,16 @@
 package main
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+// Using a struct helps us to convey our intentions
+// more clearly!
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
