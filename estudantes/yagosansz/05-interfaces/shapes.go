afd01315b0ef720135b41d651cda7f88d6a53aad
Yago Santos <yagosansz@gmail.com>
2024-11-30 19:38:11 -0500

    feat: use method instead of func

--- estudantes/yagosansz/05-interfaces/shapes.go
+++ estudantes/yagosansz/05-interfaces/shapes.go
@@ -1,10 +1,28 @@
 package main
 
+import "math"
+
 // Using a struct helps us to convey our intentions
 // more clearly!
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+// func (receiverName ReceiverType) MethodName(args)
+//
+// It's a convention that the receiverName is the first letter
+// of the ReceiverType
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
