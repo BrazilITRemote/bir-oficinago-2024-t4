d28e4dffa519d9a9076f9a6909ea395f576a94a8
Yago Santos <yagosansz@gmail.com>
2024-12-01 20:30:23 -0500

    test: use map to store tests cases

--- estudantes/yagosansz/05-interfaces/shapes_test.go
+++ estudantes/yagosansz/05-interfaces/shapes_test.go
@@ -16,26 +16,33 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	// This is defining a slice of anonymous structs.
-	// It has to be instantiated right after being defined!
-	areaTests := []struct {
-		name    string
+	// Advantages of using a map instead of slice of structs to store test cases:
+	//   1. "name" is simply the map index
+	//   2. map iteration order is random, ensure tests are independent
+	//   from one another!
+	areaTests := map[string]struct {
 		shape   Shape
 		hasArea float64
 	}{
-		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
-		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
-		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+		"Rectangle": {
+			shape: Rectangle{12, 6}, hasArea: 72.0,
+		},
+		"Circle": {
+			shape: Circle{10}, hasArea: 314.1592653589793,
+		},
+		"Triangle": {
+			shape: Triangle{12, 6}, hasArea: 36.0,
+		},
 	}
 
 	// Run specific test within the table with: "go test -run TestArea/Rectangle"
 	// or "go test -v -run TestArea/Rectangle"
-	for _, tt := range areaTests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := tt.shape.Area()
+	for name, test := range areaTests {
+		t.Run(name, func(t *testing.T) {
+			got := test.shape.Area()
 
-			if got != tt.hasArea {
-				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			if got != test.hasArea {
+				t.Errorf("%#v got %g want %g", test.shape, got, test.hasArea)
 			}
 		})
 	}
