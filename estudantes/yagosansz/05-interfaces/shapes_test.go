5d1d00e39bdc4a086f58d0a285c65b8492a216f7
Yago Santos <yagosansz@gmail.com>
2024-12-01 20:22:05 -0500

    refactor: improve tests output

--- estudantes/yagosansz/05-interfaces/shapes_test.go
+++ estudantes/yagosansz/05-interfaces/shapes_test.go
@@ -19,19 +19,24 @@ func TestArea(t *testing.T) {
 	// This is defining a slice of anonymous structs.
 	// It has to be instantiated right after being defined!
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
 	}
 
+	// Run specific test within the table with: "go test -run TestArea/Rectangle"
+	// or "go test -v -run TestArea/Rectangle"
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
 
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
