b692ed632cbceead51fa61071ac0bf6d359db0ea
Yago Santos <yagosansz@gmail.com>
2024-12-04 23:08:20 -0500

    refactor: add Bitcoin type based on int

--- estudantes/yagosansz/06-pointers/wallet.go
+++ estudantes/yagosansz/06-pointers/wallet.go
@@ -1,18 +1,20 @@
 package main
 
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // IMPORTANT: Pointer dereferencing is to get the value stored in memory!
 // We don't need to do `(*w).balance` (explicit dereference) because Go will
 // automatically do that for us.
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Technically, this one would work with receiving a copy and not a pointer,
 // but by convetion we should keep method receiver types the same for consistency!
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
