e212bb8c58ae5683d8caa602adf11280f5753882
Yago Santos <yagosansz@gmail.com>
2024-12-04 22:48:20 -0500

    doc: add comment on Deposit method

--- estudantes/yagosansz/06-pointers/wallet.go
+++ estudantes/yagosansz/06-pointers/wallet.go
@@ -6,6 +6,8 @@ type Wallet struct {
 	balance int
 }
 
+// w Wallet is a copy of the original receiver!
+// In our case the "original" receiver is coming from tests.
 func (w Wallet) Deposit(amount int) {
 	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
