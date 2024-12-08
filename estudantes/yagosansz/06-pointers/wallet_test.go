02acc9c486ab3b9b74c4bd4ceb4753a402339c79
Yago Santos <yagosansz@gmail.com>
2024-12-08 16:58:36 -0500

    fix: assert no error raised when all goes fine

--- estudantes/yagosansz/06-pointers/wallet_test.go
+++ estudantes/yagosansz/06-pointers/wallet_test.go
@@ -14,8 +14,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -53,5 +54,13 @@ func assertError(t testing.TB, got, want error) {
 	// e.g.: got.Error()
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
