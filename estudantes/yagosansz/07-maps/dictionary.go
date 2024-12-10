52143e089a843acc04dcaa254213c95f2c8bcadf
Yago Santos <yagosansz@gmail.com>
2024-12-10 01:00:38 -0500

    feat: using 2nd property of map look up!

--- estudantes/yagosansz/07-maps/dictionary.go
+++ estudantes/yagosansz/07-maps/dictionary.go
@@ -1,7 +1,16 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
-	return d[word], nil
+	// Map look up returns 2 values.
+	// The 2nd one being a boolean to indicate if the key was found!
+	definition, ok := d[word]
+
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
 }
