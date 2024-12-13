edc9a9528e8f5db558d6eec6e7806df8a22b3bce
Ygor aka ControlC <ygorth.adm@gmail.com>
2024-12-12 17:41:36 -0800

    commit #3

--- estudantes/ygorth/6-pointers-errors/wallet_test.go
+++ estudantes/ygorth/6-pointers-errors/wallet_test.go
@@ -6,31 +6,49 @@ import (
 
 func TestWallet(t *testing.T) {
 
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
 
-		wallet.Deposit(Bitcoin(10))
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
 
+		if got.Error() != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		// fmt.Printf("BIR - type of err: %T\n", err)
+		// fmt.Printf("BIR - type of err: %s\n", err)
+
+		assertError(t, err, "cannot withdraw, insufficient funds")
+		assertBalance(t, wallet, startingBalance)
+	})
+
 }
