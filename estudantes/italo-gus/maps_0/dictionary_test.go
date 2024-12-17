5434efad616814cb19c3c3a7dbf7082dfe613a4b
italo-gus <italogus@gmail.com>
2024-12-16 23:16:38 -0300

    030 maps_0 - Delcaração de tipo de dados e Erro

--- estudantes/italo-gus/maps_0/dictionary_test.go
+++ estudantes/italo-gus/maps_0/dictionary_test.go
@@ -6,29 +6,48 @@ import (
 
 /*
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
-	// meuMapa :=  map [tipo_de_dados_da_chave(índice)] tipo_de_dados_do_elemento
-	// https://go.dev/ref/spec#Map_types
-	// https://pkg.go.dev/maps
-	// https://go.dev/doc/effective_go#maps
-	// https://go.dev/blog/maps
-	//
-	received := Search(dictionary, "test")
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	received := dictionary.Search("test")
 	expected := "this is just a test"
 
-	if received != expected {
-		t.Errorf("expected %q but received %q given %q ", expected, received, "test")
-	}
+	assertStrings(t, received, expected)
 }
 */
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	received := Search(dictionary, "test")
-	expected := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		received, _ := dictionary.Search("test")
+		expected := "this is just a test"
 
-	assertStrings(t, received, expected)
+		assertStrings(t, received, expected)
+	})
+
+	/*
+		t.Run("unknown word", func(t *testing.T) {
+			_, err := dictionary.Search("unknown")
+			expected := "could not find the word you were looking for"
+
+			if err == nil {
+				t.Fatal("expected to get an error.")
+			}
+
+			assertStrings(t, err.Error(), expected)
+		})
+	*/
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, received := dictionary.Search("unknown")
+		if received == nil {
+			t.Fatal("expected to get an error.")
+		}
+		assertError(t, received, ErrNotFound)
+		// gerando erro, criando erro diferente do esperado
+		//assertError(t, received, errors.New("could not find the word you were looking for !"))
+	})
+
 }
 
 func assertStrings(t testing.TB, received, expected string) {
@@ -36,5 +55,13 @@ func assertStrings(t testing.TB, received, expected string) {
 
 	if received != expected {
 		t.Errorf("expected %q but received %q ", expected, received)
+	}
+}
+
+func assertError(t testing.TB, received, expected error) {
+	t.Helper()
+
+	if received != expected {
+		t.Errorf("got error %q expected %q", received, expected)
 	}
 }
