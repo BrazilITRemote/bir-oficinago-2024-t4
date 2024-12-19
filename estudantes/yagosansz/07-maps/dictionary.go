dac9b5005ef689a070493dbf54a810ef57d4b5a3
Yago Santos <yagosansz@gmail.com>
2024-12-18 23:36:34 -0500

    refactor: create own DictionaryErr type

--- estudantes/yagosansz/07-maps/dictionary.go
+++ estudantes/yagosansz/07-maps/dictionary.go
@@ -1,13 +1,16 @@
 package main
 
-import "errors"
-
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
 
+type DictionaryErr string
 type Dictionary map[string]string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	// Map look up returns 2 values.
@@ -20,6 +23,9 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
+// The default behaviour of a map, is to overwrite the value of an existing key.
+// Here we are making it so that the behavior is to throw an error when we try
+// to add a definition for an existing word!
 func (d Dictionary) Add(word, definition string) error {
 	_, err := d.Search(word)
 
