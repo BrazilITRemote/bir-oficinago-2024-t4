752d55ce3c21a93144b4bb0931438ac340311008
Ygor aka ControlC <ygorth.adm@gmail.com>
2025-01-07 16:55:01 -0800

    commit

--- estudantes/ygorth/9-mocking/mocking.go
+++ estudantes/ygorth/9-mocking/mocking.go
@@ -4,10 +4,31 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
