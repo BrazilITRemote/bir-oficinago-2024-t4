34fb8dcc35aacee26616042e394c352c6e038af9
Yago Santos <yagosansz@gmail.com>
2025-01-18 22:28:29 -0500

    feat: use channel to handle comms between processes

--- estudantes/yagosansz/10-concurrency/check_websites.go
+++ estudantes/yagosansz/10-concurrency/check_websites.go
@@ -1,20 +1,30 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+// Anonymous values within a structure are allowed.
+// They are useful when it's hard to know what to name a value!
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func() {
-			results[url] = wc(url)
+			// Send statement
+			resultChannel <- result{url, wc(url)}
 		}()
 	}
 
-	// give enough time for goroutines to catch up!
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		// Receive expression
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
 
 	return results
 }
