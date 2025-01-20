6e530fdbd8614b8486ed43a1319e51e826ec2e71
Yago Santos <yagosansz@gmail.com>
2025-01-19 20:14:48 -0500

    feat: implement website racer

---
+++ estudantes/yagosansz/11-select/racer_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestRacer(t *testing.T) {
+	slowURL := "https://www.facebook.com"
+	fastURL := "https://www.quii.dev"
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
