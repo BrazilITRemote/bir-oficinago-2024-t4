f010ba5ec940baa2bd9592aec16744a94a713eb1
Ygor aka ControlC <ygorth.adm@gmail.com>
2025-01-30 17:15:06 -0800

    commit

--- estudantes/ygorth/12-reflection/reflection.go
+++ estudantes/ygorth/12-reflection/reflection.go
@@ -1,9 +1,22 @@
 package reflection
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+
+	tipo := val.Type()
+	fmt.Printf("\n Tipo de valor: %s", tipo)
+
+	// if val.Kind() != reflect.Struct {
+	// 	return
+	// }
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
 }
