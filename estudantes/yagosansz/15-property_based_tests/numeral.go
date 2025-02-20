8eb1cba90d378d5f9e9961a51fa7452f0c3f9e63
Yago Santos <yagosansz@gmail.com>
2025-02-19 22:50:28 -0500

    refactor: subtract from arabic to build roman numerals

--- estudantes/yagosansz/15-property_based_tests/numeral.go
+++ estudantes/yagosansz/15-property_based_tests/numeral.go
@@ -3,12 +3,23 @@ package main
 import "strings"
 
 func ConvertToRoman(arabic int) string {
-	// A Builder is used to efficiently build a string using Write methods.
-	// It minimizes memory copying.
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
-		result.WriteString("I")
+	for arabic > 0 {
+		switch {
+		case arabic > 8:
+			result.WriteString("IX")
+			arabic -= 9
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
 	}
 
 	return result.String()
