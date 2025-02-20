8baa1d62095030772d9fec025ff16dea7f224e71
Yago Santos <yagosansz@gmail.com>
2025-02-19 22:27:47 -0500

    refactor: use builder to concatenate string

--- estudantes/yagosansz/15-property_based_tests/numeral.go
+++ estudantes/yagosansz/15-property_based_tests/numeral.go
@@ -1,9 +1,15 @@
 package main
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 2 {
-		return "II"
+	// A Builder is used to efficiently build a string using Write methods.
+	// It minimizes memory copying.
+	var result strings.Builder
+
+	for i := 0; i < arabic; i++ {
+		result.WriteString("I")
 	}
 
-	return "I"
+	return result.String()
 }
--- estudantes/yagosansz/15-property_based_tests/numeral_test.go
+++ estudantes/yagosansz/15-property_based_tests/numeral_test.go
@@ -9,7 +9,8 @@ func TestRomanNumerals(t *testing.T) {
 		Want        string
 	}{
 		{"1 gets converted to I", 1, "I"},
-		{"2 gets converted to I", 2, "II"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
 	}
 
 	for _, test := range cases {
