9d6c5f0a4d203075482ab04422a08d935b1163d3
Yago Santos <yagosansz@gmail.com>
2025-02-22 13:56:23 -0500

    refactor: add logic to convert any roman to arabic

--- estudantes/yagosansz/15-property_based_tests/numeral.go
+++ estudantes/yagosansz/15-property_based_tests/numeral.go
@@ -37,5 +37,14 @@ func ConvertToRoman(arabic int) string {
 }
 
 func ConvertToArabic(roman string) int {
-	return 1
+	var arabic = 0
+
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
+	}
+
+	return arabic
 }
