69811d08b687b2405b13cfa0e10b10101790ea05
Ygor aka ControlC <ygorth.adm@gmail.com>
2025-03-11 18:08:11 -0700

    commit

--- estudantes/ygorth/16-maths/clockface_acceptance_test.go
+++ estudantes/ygorth/16-maths/clockface_acceptance_test.go
@@ -1,5 +1,12 @@
 package clockface_test
 
+import (
+	"testing"
+	"time"
+
+	clockface "github.com/BrazilITRemote/bir-oficinago-2024-t4/estudantes/ygorth/16-maths"
+)
+
 // func TestSecondHandAtMidnight(t *testing.T) {
 // 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
@@ -11,13 +18,13 @@ package clockface_test
 // 	}
 // }
 
-// func TestSecondHandAt30Seconds(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
 
-// 	want := clockface.Point{X: 150, Y: 150 + 90}
-// 	got := clockface.SecondHand(tm)
+	want := clockface.Point{X: 150, Y: 150 + 90}
+	got := clockface.SecondHand(tm)
 
-// 	if got != want {
-// 		t.Errorf("Got %v, wanted %v", got, want)
-// 	}
-// }
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
