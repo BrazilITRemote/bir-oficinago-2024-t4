d636ac45b647d5ab6ade11a2c4a105d072f5e678
Yago Santos <yagosansz@gmail.com>
2025-03-15 21:45:05 -0400

    feat: create Post as files are being read

--- estudantes/yagosansz/17-reading_files/blogposts/blogposts.go
+++ estudantes/yagosansz/17-reading_files/blogposts/blogposts.go
@@ -1,10 +1,12 @@
 package blogposts
 
 import (
+	"io"
 	"io/fs"
 )
 
 type Post struct {
+	Title string
 }
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
@@ -13,8 +15,30 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 		return nil, err
 	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f)
+		if err != nil {
+			//todo: needs clarification, should we totally fail if one file fails? or just ignore?
+			return nil, err
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := fileSystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	// Here we are cutting out `Title: ` by slicing the string
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
 }
