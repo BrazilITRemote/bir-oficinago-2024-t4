59cc1290a8d93825b583b91ec5a9f49ec99e4a57
Ygor aka ControlC <ygorth.adm@gmail.com>
2025-04-15 18:11:25 -0700

    commit

--- estudantes/ygorth/19-generics/generics_test.go
+++ estudantes/ygorth/19-generics/generics_test.go
@@ -54,6 +54,17 @@ func TestStack(t *testing.T) {
 		AssertEqual(t, value, "123")
 		AssertTrue(t, myStackOfStrings.IsEmpty())
 	})
+
+	t.Run("interface stack DX is horrid", func(t *testing.T) {
+		myStackOfInts := new(StackOfInts)
+
+		myStackOfInts.Push(1)
+		myStackOfInts.Push(2)
+		firstNum, _ := myStackOfInts.Pop()
+		secondNum, _ := myStackOfInts.Pop()
+		AssertEqual(t, firstNum+secondNum, 3)
+	})
+
 }
 
 func AssertEqual[T comparable](t *testing.T, got, want T) {
