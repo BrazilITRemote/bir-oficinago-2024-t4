c2b19642da2c3112741a14f74c00b28d381fbafb
Ygor aka ControlC <ygorth.adm@gmail.com>
2025-04-17 19:07:22 -0700

    commit

--- estudantes/ygorth/20-arrays-slices-generics/bad_bank.go
+++ estudantes/ygorth/20-arrays-slices-generics/bad_bank.go
@@ -6,15 +6,38 @@ type Transaction struct {
 	Sum  float64
 }
 
-func BalanceFor(transactions []Transaction, name string) float64 {
-	adjustBalance := func(currentBalance float64, t Transaction) float64 {
-		if t.From == name {
-			return currentBalance - t.Sum
-		}
-		if t.To == name {
-			return currentBalance + t.Sum
-		}
-		return currentBalance
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
+
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(
+		transactions,
+		applyTransaction,
+		account,
+	)
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
 	}
-	return Reduce(transactions, adjustBalance, 0.0)
+	if transaction.To == a.Name {
+		a.Balance += transaction.Sum
+	}
+	return a
+}
+
+func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
+	for _, v := range items {
+		if predicate(v) {
+			return v, true
+		}
+	}
+	return
 }
